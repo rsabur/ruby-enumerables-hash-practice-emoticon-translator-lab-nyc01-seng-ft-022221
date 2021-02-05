2cedc9c5a04e4a990fff500f2538f8fe6cc618f4
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-05 19:06:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,9 +5,7 @@ require 'pry'
 def load_library(path)
   # code goes here
   emoticons = {}
-  #binding.pry
   YAML.load_file(path).each do |name, emoji|
-    #binding.pry
     emoticons[key] = {}
   end
   emoticons
