105a77c3ac4f9f08f6c9925e1a8f7466dbfdf833
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-05 19:50:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_english_meaning(path, emoticons)
   # code goes here
   load_library(path).each do |name, emoji|
      if emoji[:japanese] == emoticons
-       name[:english]
+       name
      end
     #binding.pry
   end
