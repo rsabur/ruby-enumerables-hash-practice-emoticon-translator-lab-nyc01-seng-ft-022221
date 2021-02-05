8602978a9d59eb9c1dee7d201f6bf819c62aaf46
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-05 19:14:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,8 @@ def load_library(path)
   emoticons = {}
   YAML.load_file(path).each do |name, emoji|
     emoticons[name] = {}
-    emoticons[name][:english]
+    emoticons[name][:english] == ""
+    emoticons[name][:japanese] == ""
   end
   emoticons
 end
